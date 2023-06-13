be9fd21e8a34f99ac93e31ae639258a3008c8cdb
Terlibin Oleg <rumblefishinfo@gmail.com>
2023-06-13 15:41:01 +0300

    Added switcher

--- src/clock.ino
+++ src/clock.ino
@@ -1,20 +1,26 @@
 #include <microDS3231.h>
 MicroDS3231 rtc;
 
-#define A 2
-#define F 3
-#define B 4
-#define D 5
-#define C 6
-#define E 7
-#define G 8
+#define A 2;
+#define F 3;
+#define B 4;
+#define D 5;
+#define C 6;
+#define E 7;
+#define G 8;
 
-#define D1 9
-#define D2 10
-#define D3 11
-#define D4 12
+#define D1 9;
+#define D2 10;
+#define D3 11;
+#define D4 12;
 
-#define BUTTON 13
+#define BUTTON 13;
+
+boolean lastButton = LOW;
+
+const int MODES_COUNT = 3;
+const char* MODES[MODES_COUNT] = { "time", "date", "temp" };
+int modeIndex = 0;
 
 void setup()
 {
@@ -41,15 +47,16 @@ void setup()
 
 void loop()
 {
- 
+  currentButton = debounce(lastButton);
 
-  if (digitalRead(BUTTON) == HIGH ) {
-    int temp = rtc.getTemperatureFloat(); 
-    int temp1 = temp / 10;
-    int temp2 = temp % 10;
+  if (lastButton == LOW && currentButton == HIGH)
+  {
+    changeMode();
+  }
 
-    showTime(0, 0, temp1, temp2);
-  } else {
+  switch (MODES[mode])
+  {
+  case "time":
     int hours = rtc.getHours();
     int minutes = rtc.getMinutes();
     int hour1 = hours / 10;
@@ -57,51 +64,81 @@ void loop()
     int minutes1 = minutes / 10;
     int minutes2 = minutes % 10;
 
-    showTime(hour1, hour2, minutes1, minutes2);
-  }
+    showNumbers(hour1, hour2, minutes1, minutes2);
+    break;
 
+  case "date":
+    int day = rtc.getDay(); 
+    int day1 = day / 10;
+    int day2 = day % 10;
+    int month = rtc.getMonth(); 
+    int month1 = month / 10;
+    int month2 = month % 10;
+
+    showNumbers(
+      day1, 
+      day2,
+      month1, 
+      month2
+    );
+    break;
   
-  // Serial.print(rtc.getHours());
-  // Serial.print(rtc.getHours());
-  // Serial.print(":");
-  // Serial.print(rtc.getMinutes());
-  // Serial.print(":");
-  // Serial.print(rtc.getSeconds());
-  // Serial.print(" ");
-  // Serial.print(rtc.getDay());
-  // Serial.print(" ");
-  // Serial.print(rtc.getDate());
-  // Serial.print("/");
-  // Serial.print(rtc.getMonth());
-  // Serial.print("/");
-  // Serial.println(rtc.getYear());
+  case "temp":
+    int temp = rtc.getTemperatureFloat(); 
+    int temp1 = temp / 10;
+    int temp2 = temp % 10;
+
+    showNumbers(
+      0, 
+      temp < 0 ? -1 : 0,
+      temp1, 
+      temp2
+    );
+    break;
   
-  // Serial.println(rtc.getTemperatureFloat());  
-  
-  // Serial.println(rtc.getTimeString());
-  // Serial.println(rtc.getDateString());
+  default:
+    break;
+  }
 }
 
-void showTime(int hour1, int hour2, int minutes1, int minutes2)
+void changeMode()
+{
+  modeIndex++;
+  modeIndex = modeIndex % MODES_COUNT;
+}
+
+boolean debounce(boolean last)
+{
+  boolean current = digitalRead(BUTTON);
+
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(BUTTON);
+    return current;
+ }
+}
+
+void showNumbers(int firstLamp, int secondLamp, int thirdLamp, int quadrupleLamp)
 {
   clearLEDs();
   pickDigit(0); 
-  pickNumber(minutes2);
+  picSymbol(quadrupleLamp);
   delay(5);
 
   clearLEDs();
   pickDigit(1); 
-  pickNumber(minutes1);
+  picSymbol(thirdLamp);
   delay(5);
 
   clearLEDs();
   pickDigit(2); 
-  pickNumber(hour2);
+  picSymbol(secondLamp);
   delay(5);
 
   clearLEDs();
   pickDigit(3); 
-  pickNumber(hour1);
+  picSymbol(firstLamp);
   delay(5);
 
   clearLEDs();
@@ -127,7 +164,7 @@ void pickDigit(int x)
   }
 }
 
-void pickNumber(int x) 
+void picSymbol(int x) 
 {
   switch (x) 
   {
@@ -150,6 +187,8 @@ void pickNumber(int x)
     case 8: eight();  
       break;
     case 9: nine();  
+      break;
+    case -1: minus();  
       break;
   }
 }
@@ -272,5 +311,16 @@ void nine()
   digitalWrite(D, HIGH);
   digitalWrite(E, LOW);
   digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+}
+
+void minus() 
+{
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
   digitalWrite(G, HIGH);
 }
